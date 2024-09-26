b2baa87d5526de8980f58e42db586c0f6cd0fef6
TMadhusha <madhusha200069@gmail.com>
2024-09-26 23:42:20 +0530

    changes

---
+++ Backend/TeaFactoryMIS/src/test/java/com/miniProject/TeaFactoryMIS/TeaFactoryMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniProject.TeaFactoryMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaFactoryMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
